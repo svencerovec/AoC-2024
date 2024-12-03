8b615e002697ffae2477e8d34eb07be13d9f97f9
Sven Cerovec <sven.cerovec00@gmail.com>
2024-12-03 10:57:30 +0100

    Funny day 3

---
+++ 3/funny_3_1.py
@@ -0,0 +1,2 @@
+import re
+print(sum([eval(match.replace("mul(", "").replace(")", "").replace(",", "*")) for match in re.findall(r"mul\(\d{1,3},\d{1,3}\)", "".join(open('input.txt', 'r').readlines()))]))
