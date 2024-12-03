721202997e662cbf505543ce0f5046e80a2f332d
Sven Cerovec <80400435+svencerovec@users.noreply.github.com>
2024-12-03 16:37:15 +0100

    Update funny_3_1.py

--- 3/funny_3_1.py
+++ 3/funny_3_1.py
@@ -1,2 +1,1 @@
-import re
-print(sum([eval(match.replace("mul(", "").replace(")", "").replace(",", "*")) for match in re.findall(r"mul\(\d{1,3},\d{1,3}\)", "".join(open('input.txt', 'r').readlines()))]))
+import re; print(sum([eval(match.replace("mul(", "").replace(")", "").replace(",", "*")) for match in re.findall(r"mul\(\d{1,3},\d{1,3}\)", "".join(open('input.txt', 'r').readlines()))]))
